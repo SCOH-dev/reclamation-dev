95f57f2384e133abd3aaf6f8c451281aa74140f9
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2024-12-10 02:32:33 -0800

    more quests add all mutations

--- .minecraft/kubejs/server_scripts/natures_aura_recipes.js
+++ .minecraft/kubejs/server_scripts/natures_aura_recipes.js
@@ -76,4 +76,70 @@ ServerEvents.recipes(event => {
             F: '#botania:mystical_flowers'
         }
     )
+
+    //new crumbling catalyst recipe
+    event.remove({ id: 'naturesaura:tree_ritual/crushing_catalyst'})
+    event.custom({
+        "type": "naturesaura:tree_ritual",
+        "ingredients": [
+            {
+                "item": "naturesaura:gold_brick"
+            },
+            {
+                "item": "naturesaura:gold_brick"
+            },
+            {
+                "item": "botania:livingrock"
+            },
+            {
+                "item": "botania:livingrock"
+            },
+            {
+                "item": "minecraft:flint"
+            },
+            {
+                "item": "minecraft:flint"
+            },
+            {
+                "item": "naturesaura:token_joy"
+            },
+            {
+                "item": "naturesaura:token_joy"
+            }
+        ],
+        "sapling": {
+            "item": "minecraft:spruce_sapling"
+        },
+        "output": {
+            "item": "naturesaura:crushing_catalyst"
+        },
+        "time": 500
+    })
+
+    //lead ingots
+    event.custom({
+        "type": "naturesaura:tree_ritual",
+        "ingredients": [
+            {
+                "item": "naturesaura:infused_iron"
+            },
+            {
+                "item": "naturesaura:infused_iron"
+            },
+            {
+                "item": "minecraft:nether_brick"
+            },
+            {
+                "item": "minecraft:nether_brick"
+            }
+        ],
+        "sapling": {
+            "item": "minecraft:spruce_sapling"
+        },
+        "output": {
+            "item": "embers:lead_ingot",
+            "count": 2
+        },
+        "time": 200
+    })
 })
