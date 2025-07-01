37f9785d08c192299a19e31f24fab776c047b7ac
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2025-07-01 01:21:01 -0700

    stop chicken jockeys and wandering traders from spawning

--- .minecraft/kubejs/server_scripts/reclamation.js
+++ .minecraft/kubejs/server_scripts/reclamation.js
@@ -3,3 +3,10 @@ EntityEvents.checkSpawn(event => {
     event.cancel()
   }
 })
+
+ServerEvents.loaded(event => {
+  if (event.server.persistentData.gameRules) return
+  event.server.gameRules.set("doTraderSpawning", false)
+
+  event.server.persistentData.gameRules = true
+})
