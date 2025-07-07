c1d1d90743447e7bd11aa5089d99d0b6479a83a5
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2025-07-07 14:14:04 -0700

    disable soulstone generation

--- generators/scratch.py
+++ generators/scratch.py
@@ -1,9 +1,48 @@
-"particle reclamation_util:colored_drip_hang 0.588 0.2 1 true ~ ~ ~ 0 0 0 0 1"
-for i in range(51):
-    offset = 3 - (i / 50 * 3)
-    y = i / 50 * 10
-    commands = [f"particle reclamation_util:colored_drip_hang 0.588 0.2 1 true ~{offset} ~{y} ~ 0 0 0 0 1",
-                f"particle reclamation_util:colored_drip_hang 0.588 0.2 1 true ~{-offset} ~{y} ~ 0 0 0 0 1",
-                f"particle reclamation_util:colored_drip_hang 0.588 0.2 1 true ~ ~{y} ~{offset} 0 0 0 0 1",
-                f"particle reclamation_util:colored_drip_hang 0.588 0.2 1 true ~ ~{y} ~{-offset} 0 0 0 0 1"]
-    print(str(commands) + ",")
+import glob, json
+
+input_files = glob.glob('input/biome/*', recursive=True)
+output_stuff = {}
+
+def temperature(temp):
+    if temp > 1:
+        return "HOT"
+    elif temp > 0.85:
+        return "WARM"
+    elif temp > 0.35:
+        return "NORMAL"
+    elif temp > 0.0:
+        return "COLD"
+    elif temp > -0.35:
+        return "ICY"
+    else:
+        return "FROZEN"
+
+def humidity(downfall):
+    if downfall > 0.85:
+        return "WET"
+    elif downfall > 0.3:
+        return "NORMAL"
+    else:
+        return "DRY"
+
+def type(climate):
+    types = []
+    if ((climate[0] == "WET") & ((climate[1] == "WARM") | (climate[1] == "HOT"))):
+        types.append("jungle")
+    if ((climate[0] == "NORMAL") & ((climate[1] == "NORMAL") | (climate[1] == "COLD"))):
+        types.append("forest")
+    if ((climate[0] == "NORMAL") & ((climate[1] == "NORMAL") | (climate[1] == "WARM"))):
+        types.append("plains")
+    if ((climate[0] == "DRY") & ((climate[1] == "HOT") | (climate[1] == "WARM"))):
+        types.append("desert")
+    return types
+
+
+for file in input_files:
+    with open(file, 'r') as f:
+        json_file = json.load(f)
+        climate = [humidity(json_file['downfall']), temperature(json_file['temperature'])]
+        output_stuff[file[12:-5]] = [climate, type(climate)]
+
+with open('output/biome/biomes.json', 'w') as out:
+    json.dump(output_stuff, out, indent=4)
